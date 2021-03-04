601cc9fdfe3e0ab3abf0f86d24ade3bab52d6cf1
dall06 <devleon.06@gmail.com>
2021-03-03 20:28:27 -0600

    New change for indexjs in pr05

--- sisgeo-pr05-web/index.js
+++ sisgeo-pr05-web/index.js
@@ -1,3 +1,12 @@
+const createMarker = (pos, map) => {
+    const marker = new google.maps.Marker({
+        position: pos,
+        map,
+        title: "Marker",
+    });
+    return marker;
+}
+
 function initMap() {
     let mapPorps = {
         center: {
@@ -10,12 +19,31 @@ function initMap() {
         lat : 141.152639, 
         lng : -341.711598,
     }
-
+    
     const dMap = document.getElementById("divMap");
     const map = new google.maps.Map(dMap, mapPorps);
-    const marker = new google.maps.Marker({
-        position: pos,
-        map,
-        title: "Marker",
-    });
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            let _pos = {
+                lat: pos.coords.latitude,
+                lmg: pos.coords.longitude,
+            }
+
+            const marker = createMarker(_pos, map);
+
+            map.setCenter(_pos);
+
+            const infoWindow = new google.maps.InfoWindow({
+                content: "Hello human, this is the contnet"
+            });
+            
+            marker.addListener("mouseover", () => {
+                infoWindow.open(map, marker)
+            })
+        });
+    } else {
+        createMarker(pos, map);
+    }
+
 }
