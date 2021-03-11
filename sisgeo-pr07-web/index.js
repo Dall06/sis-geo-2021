6289b2e08af280517e81c93f52232ac5d0007b51
dall06 <devleon.06@gmail.com>
2021-03-10 20:05:22 -0600

    New changes

---
+++ sisgeo-pr07-web/index.js
@@ -0,0 +1,21 @@
+let div_countries = document.getElementById("divCountries");
+let fun2 = () => console.log("Hi")
+
+
+fetch('data.json')
+.then((r) => {
+  r.json().then((d) => {
+    d.forEach(e => {
+      console.log(e);
+      let name = document.createElement("p");
+      name.textContent = "COUNTRY: " + e.country + ", CASES: " + e.cases;
+      div_countries.appendChild(name);
+    });
+  });
+
+
+}).catch((e) => {
+  console.log(e);
+});
+
+fun2();
