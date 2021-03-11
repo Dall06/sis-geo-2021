6598761537593dc7fec3053764dd9f3357c6440c
dall06 <devleon.06@gmail.com>
2021-03-10 20:25:21 -0600

    New changes

--- sisgeo-pr08-web/index.js
+++ sisgeo-pr08-web/index.js
@@ -2,7 +2,7 @@ let div_countries = document.getElementById("divCountries");
 let fun2 = () => console.log("Hi")
 
 
-fetch('data.json')
+fetch('https://corona.lmao.ninja/v3/covid-19/countries')
 .then((r) => {
   r.json().then((d) => {
     d.forEach(e => {
@@ -11,9 +11,12 @@ fetch('data.json')
       let row = document.createElement("div");
       row.className = "row border";
 
+      let column = document.createElement("div");
+      column.className = "col-12";
+
       let name = document.createElement("p");
       name.textContent = "COUNTRY: " + e.country + ", CASES: " + e.cases;
-      
+
       div_countries.appendChild(row);
       div_countries.appendChild(name);
     });
