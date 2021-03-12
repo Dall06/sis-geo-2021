0add37b9e7b01265a906eb2e030c2fc377a297a8
dall06 <devleon.06@gmail.com>
2021-03-12 17:40:07 -0600

    New changes

--- sisgeo-pr10-web/index.js
+++ sisgeo-pr10-web/index.js
@@ -10,41 +10,59 @@ var props = {
 
 function initMap() {
   fetch('paises.json')
-    .then(function(response) {
+    .then(function (response) {
 
-      response.json().then(function(d) {
-        const map = new google.maps.Map(document.getElementById('divMap'), props);
+      response.json().then(function (datos) {
 
-        d.forEach(function(m) {
+        const map = new google.maps.Map(document.getElementById('map'), propiedades);
+
+        console.log(datos);
+
+        datos.forEach(marcador => {
+
+
           fetch('https://corona.lmao.ninja/countries')
-            .then(function(r) {
-              r.json().then(function(countryData) {
-                countryData.forEach(function(r) {
-                  if (r.country == m.CountryName) {
-                    let info = "<strong>País:</strong> " + r.country + "<br><strong>Casos:</strong> " + 
-                                      r.cases + "<br><strong>Nuevos hoy:</strong> " + r.todayCases + 
-                                      "<br><strong>Muertes:</strong> " + r.deaths + "<br><strong>Muertes Hoy:</strong> " + 
-                                      r.todayDeaths + "<br><strong>Recuperados:</strong> " + r.recovered + 
-                                      "<br><strong>Activos:</strong> " + r.active + "<br><strong>Críticos:</strong> " + 
-                                      r.critical + "<br><strong>Casos por millón:</strong> " + r.casesPerOneMillion;
+            .then(function (respuesta) {
 
-                    let infowindow = new google.maps.InfoWindow({content: info});
+              respuesta.json().then(function (datospaises) {
+
+                datospaises.forEach(registro => {
+
+
+                  if (registro.country == marcador.CountryName) {
+
+                    var informacion = "<strong>País:</strong> " + registro.country + "<br><strong>Casos:</strong> " + registro.cases + "<br><strong>Nuevos hoy:</strong> " + registro.todayCases + "<br><strong>Muertes:</strong> " + registro.deaths + "<br><strong>Muertes Hoy:</strong> " + registro.todayDeaths + "<br><strong>Recuperados:</strong> " + registro.recovered + "<br><strong>Activos:</strong> " + registro.active + "<br><strong>Críticos:</strong> " + registro.critical + "<br><strong>Casos por millón:</strong> " + registro.casesPerOneMillion;
+
+                    var infowindow = new google.maps.InfoWindow({
+                      content: informacion
+                    });
 
                     let marker = new google.maps.Marker({
                       map: map,
-                      position: new google.maps.LatLng(m.CapitalLatitude, m.CapitalLongitude),
-                      title: m.CountryName + r.cases
+                      position: new google.maps.LatLng(marcador.CapitalLatitude, marcador.CapitalLongitude),
+                      title: marcador.CountryName + registro.cases
                     });
 
-                    marker.addListener('click', function() {
+                    marker.addListener('click', function () {
                       infowindow.open(map, marker);
                     });
+
                   }
+
+
                 });
+
               });
+
             });
+
+
+
         });
+
+
       });
+
     })
     .catch(function (error) {
       console.log('Hubo un problema con la petición Fetch:' + error.message);
