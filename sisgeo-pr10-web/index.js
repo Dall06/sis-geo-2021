e162aa6bd8326b4046634851925631850cab8f6e
dall06 <devleon.06@gmail.com>
2021-03-12 18:36:23 -0600

    New changes

--- sisgeo-pr10-web/index.js
+++ sisgeo-pr10-web/index.js
@@ -13,35 +13,35 @@ function initMap() {
   fetch('paises.json')
     .then(function (response) {
 
-      response.json().then(function (datos) {
+      response.json().then(function (data) {
 
         const map = new google.maps.Map(document.getElementById('divMap'), props);
 
-        console.log(datos);
+        console.log(data);
 
-        datos.forEach(marcador => {
+        data.forEach(m => {
 
 
           fetch('https://corona.lmao.ninja/v3/covid-19/countries')
-            .then(function (respuesta) {
+            .then(function (r) {
 
-              respuesta.json().then(function (datospaises) {
+              r.json().then(function (dc) {
 
-                datospaises.forEach(registro => {
+                dc.forEach(reg => {
 
 
-                  if (registro.country == marcador.CountryName) {
+                  if (reg.country == m.CountryName) {
 
-                    var informacion = "<strong>País:</strong> " + registro.country + "<br><strong>Casos:</strong> " + registro.cases + "<br><strong>Nuevos hoy:</strong> " + registro.todayCases + "<br><strong>Muertes:</strong> " + registro.deaths + "<br><strong>Muertes Hoy:</strong> " + registro.todayDeaths + "<br><strong>Recuperados:</strong> " + registro.recovered + "<br><strong>Activos:</strong> " + registro.active + "<br><strong>Críticos:</strong> " + registro.critical + "<br><strong>Casos por millón:</strong> " + registro.casesPerOneMillion;
+                    var i = "<strong>País:</strong> " + reg.country + "<br><strong>Casos:</strong> " + reg.cases + "<br><strong>Nuevos hoy:</strong> " + reg.todayCases + "<br><strong>Muertes:</strong> " + reg.deaths + "<br><strong>Muertes Hoy:</strong> " + reg.todayDeaths + "<br><strong>Recuperados:</strong> " + reg.recovered + "<br><strong>Activos:</strong> " + reg.active + "<br><strong>Críticos:</strong> " + reg.critical + "<br><strong>Casos por millón:</strong> " + reg.casesPerOneMillion;
 
                     var infowindow = new google.maps.InfoWindow({
-                      content: informacion
+                      content: i
                     });
 
                     let marker = new google.maps.Marker({
                       map: map,
-                      position: new google.maps.LatLng(marcador.CapitalLatitude, marcador.CapitalLongitude),
-                      title: marcador.CountryName + registro.cases
+                      position: new google.maps.LatLng(m.CapitalLatitude, m.CapitalLongitude),
+                      title: m.CountryName + reg.cases
                     });
 
                     marker.addListener('click', function () {
