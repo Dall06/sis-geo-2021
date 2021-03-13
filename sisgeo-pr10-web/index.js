49663b73d5855cd534fdf326da58f7a3874f4de5
dall06 <devleon.06@gmail.com>
2021-03-12 18:39:09 -0600

    New changes

--- sisgeo-pr10-web/index.js
+++ sisgeo-pr10-web/index.js
@@ -14,25 +14,26 @@ function initMap() {
     .then(function (response) {
 
       response.json().then(function (data) {
-
         const map = new google.maps.Map(document.getElementById('divMap'), props);
 
         console.log(data);
 
         data.forEach(m => {
-
-
           fetch('https://corona.lmao.ninja/v3/covid-19/countries')
             .then(function (r) {
-
               r.json().then(function (dc) {
-
                 dc.forEach(reg => {
-
-
                   if (reg.country == m.CountryName) {
-
-                    var i = "<strong>País:</strong> " + reg.country + "<br><strong>Casos:</strong> " + reg.cases + "<br><strong>Nuevos hoy:</strong> " + reg.todayCases + "<br><strong>Muertes:</strong> " + reg.deaths + "<br><strong>Muertes Hoy:</strong> " + reg.todayDeaths + "<br><strong>Recuperados:</strong> " + reg.recovered + "<br><strong>Activos:</strong> " + reg.active + "<br><strong>Críticos:</strong> " + reg.critical + "<br><strong>Casos por millón:</strong> " + reg.casesPerOneMillion;
+                    var i = "<strong>País:</strong> " + 
+                            reg.country + "<br><strong>Casos:</strong> " + 
+                            reg.cases + "<br><strong>Nuevos hoy:</strong> " + 
+                            reg.todayCases + "<br><strong>Muertes:</strong> " + 
+                            reg.deaths + "<br><strong>Muertes Hoy:</strong> " + 
+                            reg.todayDeaths + "<br><strong>Recuperados:</strong> " + 
+                            reg.recovered + "<br><strong>Activos:</strong> " + 
+                            reg.active + "<br><strong>Críticos:</strong> " + 
+                            reg.critical + "<br><strong>Casos por millón:</strong> " + 
+                            reg.casesPerOneMillion;
 
                     var infowindow = new google.maps.InfoWindow({
                       content: i
@@ -47,23 +48,12 @@ function initMap() {
                     marker.addListener('click', function () {
                       infowindow.open(map, marker);
                     });
-
                   }
-
-
                 });
-
               });
-
             });
-
-
-
         });
-
-
       });
-
     })
     .catch(function (error) {
       console.log('Hubo un problema con la petición Fetch:' + error.message);
