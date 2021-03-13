3eb50ca5c0eb3dae1f9157a143b3c27105234f3e
dall06 <devleon.06@gmail.com>
2021-03-12 18:10:17 -0600

    New changes

--- sisgeo-pr10-web/index.js
+++ sisgeo-pr10-web/index.js
@@ -10,19 +10,35 @@ var props = {
 
 function initMap() {
   fetch('paises.json')
-  .then(function(r) {
-    r.json().then(function(data) {
-      const map = new google.maps.Map(document.getElementById("divMap"), props);
+    .then(function (r) {
+      r.json().then(function (data) {
+        const map = new google.maps.Map(document.getElementById("divMap"), props);
 
-      data.forEach(m => {
-        fetch('https://corona.lmao.ninja/countries')
-        .then(function(fr) {
-          console.log(fr);
-
-
-        })
-      });
+        data.forEach(m => {
+          fetch('https://corona.lmao.ninja/countries')
+            .then(function (fr) {
+              // console.log(fr);
+              fr.json().then(function (dc) {
+                dc.forEach((reg) => {
+                  var info = "<strong>País:</strong> "+ reg.country +"<br><strong>Casos:</strong> " + reg.cases + "<br><strong>Nuevos hoy:</strong> " + reg.todayCases + "<br><strong>Muertes:</strong> " + reg.deaths + "<br><strong>Muertes Hoy:</strong> " + reg.todayDeaths + "<br><strong>Recuperados:</strong> " + reg.recovered + "<br><strong>Activos:</strong> " + reg.active + "<br><strong>Críticos:</strong> " + reg.critical + "<br><strong>Casos por millón:</strong> " + reg.casesPerOneMillion;
+                  var infoWindow = new google.maps.InfoWindow({
+                    content: info,
+                  });
+                  if (dc.country == m.CountryName) {
+                    let marker = new google.maps.Marker({
+                      map: map,
+                      position: new google.maps.LatLng(marcador.CapitalLatitude, marcador.CapitalLongitude),
+                      title: marcador.CountryName + reg.cases
+                    });
+                    marker.addListener('click', function () {
+                      infoWindow.open(map, marker);
+                    });
+                  }
+                })
+              })
+            })
+        });
+      })
     })
-  })
-  .catch(function(e) {console.log(e);})
+    .catch(function (e) { console.log(e); })
 };
