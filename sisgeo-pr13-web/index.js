ca71587161900fb80fc918761bc3d2e3409f5c49
dall06 <devleon.06@gmail.com>
2021-03-24 19:42:20 -0600

    add: pr13

--- sisgeo-pr13-web/index.js
+++ sisgeo-pr13-web/index.js
@@ -1,57 +1,5 @@
 var map;
 
-var styleMapType = new google.maps.styleMapType([
-    {
-      elementType: 'geometry',
-      stylers: [{
-        color: '#77a4ed',
-      }],
-    },
-    {
-      elementType: 'labels.text.stroke',
-      stylers: [{
-        color: '#e0b9eb',
-      }],
-    },
-    {
-      elementType: 'labels.text.fill',
-      stylers: [{
-        color: '#bcebb2',
-      }],
-    },
-    {
-      featureType: 'poi',
-      elementType: 'labels.text.fill',
-      stylers: [{
-        color: '#bcebb2',
-      }],
-    },
-    {
-      featureType: 'poi.park',
-      elementType: 'geometry',
-      stylers: [{
-        color: '#28ad37',
-      }],
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry.stroke',
-      stylers: [{
-        color: '#9caab5',
-      }],
-    },
-    {
-      featureType: 'road',
-      elementType: 'labels.text.fill',
-      stylers: [{
-        color: '#e0b9eb',
-      }],
-    },
-  ], {
-    name: 'Dark mode map',
-  }
-);
-
 let coords = {
   lat: -31.563910, lng: 147.154312
 }
@@ -59,13 +7,259 @@ let props = {
   center: coords,
   zoom: 14,
   // disableDefaultUI: true,
-  mapTypeControlOptions: [ 'roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
+  mapTypeControlOptions: ['roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
 }
 let divMap = document.getElementById('divMap');
 
 document.head.appendChild(script);
 
 function initMap() {
+  var styleMapType = new google.maps.styleMapType([
+    {
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#8ec3b9"
+        }
+      ]
+    },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1a3646"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.country",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#4b6878"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#64779e"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.province",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#4b6878"
+        }
+      ]
+    },
+    {
+      featureType: "landscape.man_made",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#334e87"
+        }
+      ]
+    },
+    {
+      featureType: "landscape.natural",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#283d6a"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#6f9ba5"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#3C7680"
+        }
+      ]
+    },
+    {
+      featureType: "poi.school",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#ff40ff"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#304a7d"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#98a5be"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#2c6675"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#255763"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#b0d5ce"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#98a5be"
+        }
+      ]
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "transit.line",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#283d6a"
+        }
+      ]
+    },
+    {
+      featureType: "transit.station",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#3a4762"
+        }
+      ]
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#0e1626"
+        }
+      ]
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#4e6d70"
+        }
+      ]
+    }
+  ], {
+    name: 'Dark mode map',
+  }
+  );
+
   map = new google.maps.Map(divMap, props);
 
   map.mapTypes.set('style_map', styleMapType);
