4f4d7f9ebf10cc03f9204ebde7ade1bd8012704c
dall06 <devleon.06@gmail.com>
2021-03-24 19:35:18 -0600

    add: pr13

---
+++ sisgeo-pr13-web/index.js
@@ -0,0 +1,61 @@
+var map;
+
+var styleMapType = new google.maps.styleMapType([
+    {
+      elementType: 'geometry',
+      stylers: [{
+        color: '#77a4ed',
+      }],
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [{
+        color: '#e0b9eb',
+      }],
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#bcebb2',
+      }],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{
+        color: '#bcebb2',
+      }],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{
+        color: '#28ad37',
+      }],
+    },
+  ], {
+    name: 'Dark mode map',
+  }
+);
+
+let coords = {
+  lat: -31.563910, lng: 147.154312
+}
+let props = {
+  center: coords,
+  zoom: 14,
+  // disableDefaultUI: true,
+  mapTypeControlOptions: [ 'roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
+}
+let divMap = document.getElementById('divMap');
+
+document.head.appendChild(script);
+
+function initMap() {
+  map = new google.maps.Map(divMap, props);
+
+  map.mapTypes.set('style_map', styleMapType);
+  map.setMapTypeId('style_map');
+
+
+};
