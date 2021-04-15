50d81ca87c9e21344c0d8a49679e5b1da58970b2
dall06 <devleon.06@gmail.com>
2021-04-14 20:01:12 -0500

    add: pr 15

---
+++ sisgeo-pr15-web/index.js
@@ -0,0 +1,76 @@
+let coords = { lat: 21.152639, lng: -101.711598 };
+let limits = {
+  north: 21.390039,
+  south: 20.858414,
+  west: -102.149631,
+  east: -101.092990
+}
+let props = [];
+
+function initMap() {
+  // m1
+  props.push({
+    center: coords,
+    zoom: 12
+  });
+  // m2
+  props.push({
+    center: coords,
+    zoom: 12,
+    disableDefaultUI: true
+  });
+  // m3
+  props.push({
+    center: coords,
+    zoom: 12,
+    zoomControl: false,
+    scaleControl: false
+  });
+  // m4
+  props.push({
+    center: coords,
+    zoom: 12,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ['roadmap', 'satellite', 'terrain']
+    }
+  });
+  // m5
+  props.push({
+    center: coords,
+    zoom: 12,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP
+    },
+    fullscreenControl: true
+  });
+    // m6
+    props.push({
+      center: coords,
+      zoom: 12,
+      restriction: {
+        latLngBounds: limits,
+        strictBounds: false
+      }
+    });
+
+  var map_1 = new google.maps.Map(document.getElementById('divMap_1'), props[0]);
+  var map_2 = new google.maps.Map(document.getElementById('divMap_2'), props[1]);
+  var map_3 = new google.maps.Map(document.getElementById('divMap_3'), props[2]);
+  var map_4 = new google.maps.Map(document.getElementById('divMap_4'), props[3]);
+  var map_5 = new google.maps.Map(document.getElementById('divMap_5'), props[4]);
+  var map_6 = new google.maps.Map(document.getElementById('divMap_6'), props[5]);
+
+}
