a1e2ef815f0813ed98099422d49d00c0c3b34658
Dall06 <devleon.06@gmail.com>
2021-05-23 23:29:01 -0500

    add: final project

--- sisgeo-prfinal-web/constants.js
+++ sisgeo-prfinal-web/constants.js
@@ -1,3 +1,4 @@
 const SignUpForm = document.getElementById("signupForm");
 const LoginForm = document.getElementById("loginForm");
 const SongsList = document.getElementById('songs-list');
+const AccountInfoCard = document.getElementById('account-info-card');
--- sisgeo-prfinal-web/view_controller/fetchAccountInfo.js
+++ sisgeo-prfinal-web/view_controller/fetchAccountInfo.js
@@ -1,6 +1,5 @@
 const ConfigureMenu = (user) => {
     if (user) {
-        console.log(user)
         if (isGoogleLogged == false) {
             database.collection('users').doc(user.uid).get().then(doc => {
                 const html = `
@@ -9,66 +8,24 @@ const ConfigureMenu = (user) => {
                     <p>Phone: ${doc.data().phone}</p>
                     <p>Address: ${doc.data().address}</p>
                 `;
-                FetchInfoDiv.innerHTML = html;
+                AccountInfoCard.innerHTML = html;
             });
         }
-
-        loggedInItems.forEach(item => item.style.display = 'block');
-        loggedOutItems.forEach(item => item.style.display = 'none');
     }
     else {
-        FetchInfoDiv.innerHTML = '';
-        loggedInItems.forEach(item => item.style.display = 'none');
-        loggedOutItems.forEach(item => item.style.display = 'block');
-    }
-}
-
-const GetDishes = (data) => {
-    if (data.length) {
-        let innerHTMLDiv = '';
-        data.forEach(document => {
-            const docData = document.data();
-            const column = `
-            <div class="card" style="width: 18rem;">
-                <img src="img/${docData.img}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Name: ${docData.name}</h5>
-                <p>$${docData.price}</p>
-                <p class="card-text">Price</p>
-                </div>
-            </div> `;
-            innerHTMLDiv += column;
-        });
-        ListFood.innerHTML = innerHTMLDiv;
-    }
-    else {
-        ListFood.innerHTML = '<p class="text-center">Ingrese con sus claves para ver los platillos.</p>';
+        AccountInfoCard.innerHTML = '';
     }
 }
 
 auth.onAuthStateChanged((user) => {
     if (user) {
         database.collection('food').onSnapshot(snapshot => {
-            GetDishes(snapshot.docs);
             ConfigureMenu(user);
         }, err => {
             console.log(err.message);
         });
-
-
-        var name, email, photoUrl, uid, emailVerified;
-
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        emailVerified = user.emailVerified;
-        uid = user.uid;
-
-        console.log(name, email, photoUrl, emailVerified, uid);
-
+        return;
     }
-    else {
-        GetDishes([]);
-        ConfigureMenu();
-    }
+    ConfigureMenu();
+    return;
 });
